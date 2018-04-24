31b4bc4efa450cc24f45962848975f7bf2b459b1
Daniel Lecoq <daninstockings@gmail.com>
2018-04-24 10:54:09 +0200

    Update controller

--- src/main/java/com/example/herokutest/TestController.java
+++ src/main/java/com/example/herokutest/TestController.java
@@ -1,9 +1,11 @@
 package com.example.herokutest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-public class Controller {
+@Controller
+public class TestController {
     @GetMapping("/")
     public ModelAndView index() {
         return new ModelAndView("index.html");
