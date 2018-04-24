15beb49535a1f7fc25fa2e035766ea43beaf33fd
Daniel Lecoq <daninstockings@gmail.com>
2018-04-24 11:46:13 +0200

    Setup database connection

--- src/main/java/com/example/herokutest/TestController.java
+++ src/main/java/com/example/herokutest/TestController.java
@@ -1,13 +1,62 @@
 package com.example.herokutest;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
 
 @Controller
 public class TestController {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
+
     @GetMapping("/")
     public ModelAndView index() {
         return new ModelAndView("index.html");
+    }
+
+    @RequestMapping("/db")
+    String db(Map<String, String> model) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM public.Product");
+
+            String output = "";
+            while (rs.next()) {
+                output = rs.toString();
+            }
+
+            model.put("records", output);
+            return "db";
+        } catch (Exception e) {
+            model.put("message", e.getMessage());
+            return "error";
+        }
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
     }
 }
